045029065292fb5472e1beae9a402a8ae7716f0f
ovie009 <komitexstock@gmail.com>
2024-02-05 20:40:51 +0100

    Play video ontouchstart and onclick interaction

--- main.js
+++ main.js
@@ -2,15 +2,28 @@
 // Select the video element with id #video
 const video = document.getElementById('video');
 
+// Function to play the video
+function playVideo() {
+    video.play();
+}
+
+// Add touch event listener to the document, then play the video
+document.addEventListener('touchstart', playVideo);
+
 // Delay the video playback by 1500 milliseconds (1.5 seconds)
 setTimeout(function() {
-  video.play();
+    playVideo
 }, 500);
 
 // check if video is playing every 2 seconds
 // if its not play video
 setInterval(() => {
     if (video.paused) {
-        video.play();
+        playVideo
     }
 }, 3000);
+
+// onclick play video
+document.addEventListener('click', function() {
+    playVideo
+});
